2e9fdc4a74e037a0bde5fa4a50d7e8179d5b3eaa
Devinda Thoradeniya <devindathoradeniya@gmail.com>
2026-03-01 23:10:07 +0530

    Update content.tsx

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -2,14 +2,14 @@ import { About, Blog, Gallery, Home, Newsletter, Person, Social, Work } from "@/
 import { Line, Row, Text } from "@once-ui-system/core";
 
 const person: Person = {
-  firstName: "Selene",
-  lastName: "Yu",
-  name: `Selene Yu`,
-  role: "Design Engineer",
+  firstName: "Devinda",
+  lastName: "Thoradeniya",
+  name: `Devinda Thoradeniya`,
+  role: "Systems Engineer",
   avatar: "/images/avatar.jpg",
-  email: "example@gmail.com",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  email: "devindathoradeniya@gmail.com",
+  location: "Asia/Sri Lanka", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["English", "Sinhalese"], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter: Newsletter = {
