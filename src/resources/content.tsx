8bf196a883df044252d9eaaae5d949514e1dedd2
Devinda Thoradeniya <devindathoradeniya@gmail.com>
2026-03-02 08:42:53 +0530

    Update content.tsx

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -355,8 +355,8 @@ const blog: Blog = {
   label: "Blog",
   title: "Writing about design and tech...",
   description: `Read what ${person.name} has been up to recently`,
-  Create new blog posts by adding a new .mdx file to app/blog/posts
-  All posts will be listed on the /blog route
+  // Create new blog posts by adding a new .mdx file to app/blog/posts
+  // All posts will be listed on the /blog route
 };
 
 const work: Work = {
